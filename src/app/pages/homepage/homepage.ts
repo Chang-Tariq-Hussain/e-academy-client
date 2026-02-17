904a6b726441c02cf3c7a6881b4a51721c49c942
Tariq Chang <chang.tariq48@gmail.com>
2026-02-17 19:05:33 +0500

    added all the section with sticky header

--- src/app/pages/homepage/homepage.ts
+++ src/app/pages/homepage/homepage.ts
@@ -3,10 +3,14 @@ import { AppConfig } from '../../core/app-config';
 import { Footer } from '../../shared/layout/footer/footer';
 import { Header } from '../../shared/layout/header/header';
 import { NzButtonWrapperComponent } from '../../shared/ui/nz-button-wrapper/nz-button-wrapper.component';
+import { About } from '../about/about';
+import { Contact } from '../contact/contact';
+import { Courses } from '../courses/courses';
+import { Instructors } from '../instructors/instructors';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Header, Footer, NzButtonWrapperComponent],
+  imports: [Header, Footer, NzButtonWrapperComponent, About, Instructors, Contact, Courses],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss',
 })
