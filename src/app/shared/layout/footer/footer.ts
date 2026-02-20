a3218cc809d88f616b6669000769dac4f0b5ccbf
Tariq Chang <chang.tariq48@gmail.com>
2026-02-20 22:07:57 +0500

    completed footer design and fixed center view all button issue

--- src/app/shared/layout/footer/footer.ts
+++ src/app/shared/layout/footer/footer.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppConfig } from '../../../core/app-config';
+import { NzButtonWrapperComponent } from '../../ui/nz-button-wrapper/nz-button-wrapper.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NzButtonWrapperComponent],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  config = inject(AppConfig);
 }
